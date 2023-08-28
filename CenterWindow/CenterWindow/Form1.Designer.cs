12e4fada620310997befea92202860222f06de81
AirSoftick <141844045+AirSoftick@users.noreply.github.com>
2023-08-28 20:05:26 +0300

    CenterWindow

---
+++ CenterWindow/CenterWindow/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CenterWindow
+{
+    partial class WC
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WC));
+            this.comboBoxPosition = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.bttStart = new Guna.UI2.WinForms.Guna2Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.PanelControll = new System.Windows.Forms.Panel();
+            this.bttClose = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.DragControl = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.Elipse = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.MainPanel.SuspendLayout();
+            this.PanelControll.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBoxPosition
+            // 
+            this.comboBoxPosition.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.comboBoxPosition.BorderThickness = 0;
+            this.comboBoxPosition.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBoxPosition.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPosition.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBoxPosition.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBoxPosition.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.comboBoxPosition.ForeColor = System.Drawing.Color.DimGray;
+            this.comboBoxPosition.ItemHeight = 30;
+            this.comboBoxPosition.Location = new System.Drawing.Point(3, 35);
+            this.comboBoxPosition.Name = "comboBoxPosition";
+            this.comboBoxPosition.Size = new System.Drawing.Size(315, 36);
+            this.comboBoxPosition.TabIndex = 0;
+            // 
+            // bttStart
+            // 
+            this.bttStart.Animated = true;
+            this.bttStart.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.bttStart.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.bttStart.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.bttStart.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.bttStart.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(40)))), ((int)(((byte)(80)))));
+            this.bttStart.Font = new System.Drawing.Font("Verdana", 11.25F);
+            this.bttStart.ForeColor = System.Drawing.Color.White;
+            this.bttStart.Location = new System.Drawing.Point(3, 77);
+            this.bttStart.Name = "bttStart";
+            this.bttStart.Size = new System.Drawing.Size(315, 40);
+            this.bttStart.TabIndex = 1;
+            this.bttStart.Text = "ЗАПУСТИТЬ";
+            this.bttStart.Click += new System.EventHandler(this.bttStart_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.MainPanel.Controls.Add(this.PanelControll);
+            this.MainPanel.Controls.Add(this.bttStart);
+            this.MainPanel.Controls.Add(this.comboBoxPosition);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(322, 125);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // PanelControll
+            // 
+            this.PanelControll.BackColor = System.Drawing.Color.SlateBlue;
+            this.PanelControll.Controls.Add(this.bttClose);
+            this.PanelControll.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelControll.Location = new System.Drawing.Point(0, 0);
+            this.PanelControll.Name = "PanelControll";
+            this.PanelControll.Size = new System.Drawing.Size(322, 29);
+            this.PanelControll.TabIndex = 2;
+            // 
+            // bttClose
+            // 
+            this.bttClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bttClose.FillColor = System.Drawing.Color.SlateBlue;
+            this.bttClose.IconColor = System.Drawing.Color.White;
+            this.bttClose.Location = new System.Drawing.Point(277, 0);
+            this.bttClose.Name = "bttClose";
+            this.bttClose.Size = new System.Drawing.Size(45, 29);
+            this.bttClose.TabIndex = 0;
+            this.bttClose.Click += new System.EventHandler(this.bttClose_Click);
+            // 
+            // DragControl
+            // 
+            this.DragControl.DockIndicatorTransparencyValue = 0.6D;
+            this.DragControl.TargetControl = this.PanelControll;
+            this.DragControl.TransparentWhileDrag = false;
+            // 
+            // Elipse
+            // 
+            this.Elipse.BorderRadius = 8;
+            this.Elipse.TargetControl = this;
+            // 
+            // WC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 125);
+            this.Controls.Add(this.MainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WC";
+            this.Text = "WC";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WC_FormClosing);
+            this.MainPanel.ResumeLayout(false);
+            this.PanelControll.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ComboBox comboBoxPosition;
+        private Guna.UI2.WinForms.Guna2Button bttStart;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Panel PanelControll;
+        private Guna.UI2.WinForms.Guna2ControlBox bttClose;
+        private Guna.UI2.WinForms.Guna2DragControl DragControl;
+        private Guna.UI2.WinForms.Guna2Elipse Elipse;
+    }
+}
+
---
+++ CenterWindow/CenterWindow/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CenterWindow
+{
+    public partial class WC : Form
+    {
+        private int shiftCount = 0;
+        private Thread thread;
+        private int savedWidth;
+        private int savedHeight;
+        private bool isRunning = false;
+
+        public WC()
+        {
+            InitializeComponent();
+
+            string[] positions = { "Центр", "Центр верхний", "Центр нижний", "Правый верхний угол", "Правый нижний угол", "Левый верхний угол", "Левый нижний угол" };
+            comboBoxPosition.Items.AddRange(positions);
+
+            comboBoxPosition.SelectedIndex = 0;
+
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Открыть", TrayOpen_Click);
+            trayMenu.MenuItems.Add("Выход", TrayExit_Click);
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Icon = Properties.Resources.IconProgramm;
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible = true;
+
+            trayIcon.Visible = false;
+        }
+
+        private void bttStart_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                bttStart.Text = "ОСТАНОВИТЬ";
+
+                thread = new Thread(WaitForShiftKeyPress);
+                thread.Start();
+            }
+            else
+            {
+                isRunning = false;
+                bttStart.Text = "ЗАПУСТИТЬ";
+            }
+        }
+
+        private void WaitForShiftKeyPress()
+        {
+            while (isRunning && shiftCount < 3)
+            {
+                if (IsShiftKeyPressed())
+                {
+                    shiftCount++;
+                }
+                else
+                {
+                    shiftCount = 0;
+                }
+
+                Thread.Sleep(100);
+            }
+
+            if (isRunning)
+            {
+                Invoke(new Action(() =>
+                {
+                    AlignWindow();
+                }));
+
+                shiftCount = 0;
+
+                WaitForShiftKeyPress();
+            }
+        }
+
+        private bool IsShiftKeyPressed()
+        {
+            short shiftState = GetKeyState(VirtualKeyStates.VK_SHIFT);
+            return (shiftState & 0x8000) != 0;
+        }
+
+        private void AlignWindow()
+        {
+            IntPtr activeWindowHandle = GetForegroundWindow();
+
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+
+            RECT windowRect;
+            GetWindowRect(activeWindowHandle, out windowRect);
+            int windowWidth = windowRect.right - windowRect.left;
+            int windowHeight = windowRect.bottom - windowRect.top;
+
+            savedWidth = windowWidth;
+            savedHeight = windowHeight;
+
+            string selectedPosition = comboBoxPosition.SelectedItem.ToString();
+
+            switch (selectedPosition)
+            {
+                case "Центр":
+                    SetWindowPos(activeWindowHandle, IntPtr.Zero, (screenWidth / 2) - (windowWidth / 2), (screenHeight / 2) - (windowHeight / 2), windowWidth, windowHeight, 0);
+                    break;
+                case "Центр верхний":
+                    SetWindowPos(activeWindowHandle, IntPtr.Zero, (screenWidth / 2) - (windowWidth / 2), 0, windowWidth, windowHeight, 0);
+                    break;
+                case "Центр нижний":
+                    SetWindowPos(activeWindowHandle, IntPtr.Zero, (screenWidth / 2) - (windowWidth / 2), screenHeight - windowHeight, windowWidth, windowHeight, 0);
+                    break;
+                case "Правый верхний угол":
+                    SetWindowPos(activeWindowHandle, IntPtr.Zero, screenWidth - windowWidth, 0, windowWidth, windowHeight, 0);
+                    break;
+                case "Правый нижний угол":
+                    SetWindowPos(activeWindowHandle, IntPtr.Zero, screenWidth - windowWidth, screenHeight - windowHeight, windowWidth, windowHeight, 0);
+                    break;
+                case "Левый верхний угол":
+                    SetWindowPos(activeWindowHandle, IntPtr.Zero, 0, 0, windowWidth, windowHeight, 0);
+                    break;
+                case "Левый нижний угол":
+                    SetWindowPos(activeWindowHandle, IntPtr.Zero, 0, screenHeight - windowHeight, windowWidth, windowHeight, 0);
+                    break;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern short GetKeyState(VirtualKeyStates virtualKeyCode);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        private enum VirtualKeyStates : int
+        {
+            VK_SHIFT = 0x10,
+        }
+
+        private struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+
+        private NotifyIcon trayIcon;
+        private ContextMenu trayMenu;
+
+        private void bttClose_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ShowForm()
+        {
+            this.Visible = true;
+            this.ShowInTaskbar = true;
+        }
+
+        private void HideForm()
+        {
+            this.Visible = false;
+            this.ShowInTaskbar = false;
+        }
+
+        private void WC_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                HideForm();
+                trayIcon.Visible = true;
+            }
+            else
+            {
+                base.OnFormClosing(e);
+            }
+        }
+
+        private void TrayOpen_Click(object sender, EventArgs e)
+        {
+            ShowForm();
+            trayIcon.Visible = false;
+        }
+
+        private void TrayExit_Click(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
